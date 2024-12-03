91614844c054ad685e90c1bf032e23db94adc39d
Iago <alo@gmail.com>
2024-12-03 20:26:33 -0300

    subida prova 2 chama

---
+++ todos/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView, CreateView, UpdateView
+from .models import Todo
+from django.urls import reverse_lazy
+
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url= reverse_lazy('todo_list')
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title','deadline']
+    success_url = reverse_lazy('todo_list')
